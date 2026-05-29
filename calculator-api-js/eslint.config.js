27a2f31e8c4b1f9562c0ae8c9c6c549150ff43bc
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2026-05-29 10:44:35 +0200

    feat: jest config

--- calculator-api-js/eslint.config.js
+++ calculator-api-js/eslint.config.js
@@ -1,1 +1,21 @@
-// configuration eslint
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
--- calculator-api-js/jest.config.js
+++ calculator-api-js/jest.config.js
@@ -1,2 +1,13 @@
-// configuration jest
-module.exports = {};
+module.exports = {
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.js"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
