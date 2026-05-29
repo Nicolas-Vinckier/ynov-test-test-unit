809220a85ee79e0d4f99b7742e6c245baf454db5
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2026-05-29 10:47:51 +0200

    feat: add calculator

--- calculator-api-js/src/calculator.js
+++ calculator-api-js/src/calculator.js
@@ -1,1 +1,22 @@
-// logique métier (étape 2)
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error("Division par zéro impossible.");
+    }
+    return a / b;
+  }
+}
+
+module.exports = Calculator;
